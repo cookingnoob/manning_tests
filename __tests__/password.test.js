5510a9f722d7ab7a2f92662828c002be01ddaf18
alan <alan@iMac-de-alan.local>
2024-07-16 23:54:11 -0600

    add exercises up to 2.6 which is the beforeEach part

--- __tests__/password.test.js
+++ __tests__/password.test.js
@@ -1,11 +1,48 @@
-const verifyPassword = require("../password");
-describe("verifyPassword", () => {
+const PasswordVerifier1 = require("../password");
+
+describe("PasswordVerifier1", () => {
+  let verifier;
+  beforeEach(() => (verifier = new PasswordVerifier1()));
   describe("Given a failing rule", () => {
-    const fakeRule = (input) => ({ passed: false, reason: "fake reason" });
+    let fakeRule, errors;
+    beforeEach(() => {
+      fakeRule = (input) => ({ passed: false, reason: "fake reason" });
+      verifier.addRule(fakeRule);
+      errors = verifier.verify("any value");
+    });
 
-    test("Returns errors", () => {
-      const errors = verifyPassword("any value", [fakeRule]);
+    test("has an error message based on the rule.reason", () => {
+      expect(errors[0]).toContain("fake reason");
+    });
 
+    test("has exactly one error", () => {
+      expect(errors.length).toBe(1);
+    });
+  });
+  describe("with a passing rule", () => {
+    let fakeRule, errors;
+    beforeEach(() => {
+      fakeRule = (input) => ({ passed: true, reson: "" });
+      verifier.addRule(fakeRule);
+      errors = verifier.verify("any value");
+    });
+    it("has no errors", () => {
+      expect(errors.length).toBe(0);
+    });
+  });
+  describe("with a failing and a passing rule", () => {
+    let fakeRulePass, fakeRuleFail, errors;
+    beforeEach(() => {
+      fakeRulePass = (input) => ({ passed: true, reason: "fake success" });
+      fakeRuleFail = (input) => ({ passed: false, reason: "fake reason" });
+      verifier.addRule(fakeRulePass);
+      verifier.addRule(fakeRuleFail);
+      errors = verifier.verify("any value");
+    });
+    it("has one error", () => {
+      expect(errors.length).toBe(1);
+    });
+    it("error text belongs to failed rule", () => {
       expect(errors[0]).toContain("fake reason");
     });
   });
--- password.js
+++ password.js
@@ -1,11 +1,21 @@
-const verifyPassword = (input, rules) => {
-  const errors = [];
-  rules.forEach((rule) => {
-    const result = rule(input);
-    if (!result.passed) {
-      errors.push(`error ${result.reason}`);
-    }
-  });
-  return errors;
-};
-module.exports = verifyPassword;
+class PasswordVerifier1 {
+  constructor() {
+    this.rules = [];
+  }
+  addRule(rule) {
+    this.rules.push(rule);
+  }
+
+  verify(input) {
+    const errors = [];
+    this.rules.forEach((rule) => {
+      const result = rule(input);
+      if (!result.passed) {
+        errors.push(`error ${result.reason}`);
+      }
+    });
+    return errors;
+  }
+}
+
+module.exports = PasswordVerifier1;
