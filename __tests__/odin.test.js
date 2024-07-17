5ece0496a3771cdacaab604a18259ec25c0e8dba
alan <alan@iMac-de-alan.local>
2024-07-17 17:22:24 -0600

    add second exercise of odins HW

--- __tests__/odin.test.js
+++ __tests__/odin.test.js
@@ -1,7 +1,15 @@
-const { capitalize } = require("../odin");
+const { capitalize, reverseString } = require("../odin");
+
 describe("capitalize function", () => {
-  test("takes a string and returns it with the first character capitalized", () => {
+  test("returns string with the first character capitalized", () => {
     const ans = "Hello world";
     expect(capitalize("hello world")).toBe(ans);
+  });
+});
+
+describe("reverseString function", () => {
+  test("returns string reversed", () => {
+    const ans = "dlrow olleh";
+    expect(reverseString("hello world")).toBe(ans);
   });
 });
--- odin.js
+++ odin.js
@@ -2,4 +2,17 @@ const capitalize = (stringToWork) => {
   return stringToWork[0].toUpperCase() + stringToWork.slice(1);
 };
 
-module.exports = { capitalize };
+const reverseString = (stringToWork) => {
+  let left = 0;
+  let right = stringToWork.length - 1;
+  let ans = [];
+  while (left <= right) {
+    ans[right] = stringToWork.charAt(left);
+    ans[left] = stringToWork.charAt(right);
+    left++;
+    right--;
+  }
+  return ans.join("");
+};
+
+module.exports = { capitalize, reverseString };
